50a680ce53448ad0b10bce5fb42bcd74599f3290
Alexander R Torrijos <artorrijos@gmail.com>
2023-04-16 10:35:44 +0800

    Web server working with basic request response

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rust
+
+## Web Server
+
+Building a Web Server in Rust - Part 1
+
+[Building a Web Server in Rust - Part 1 - YouTube](https://www.youtube.com/watch?v=BHxmWTVFWxQ)
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    fmt::Error,
+    io::prelude::*,
     net::{TcpListener, TcpStream},
 };
 
@@ -8,7 +8,28 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
+        handle_connection(stream);
         println!("Connection established!");
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    // Ok(())
+
+    // HTTP-Version Status-Code Reason-Phrase CRLF
+    // headers CRLF
+    // message-body
+    //
+    // ex: HTTP/1.1 200 OK\r\n\r\n
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
