5f2455f7643a68293dbc1b83d5edcaab9c71b065
Alexander R Torrijos <artorrijos@gmail.com>
2023-04-16 10:22:43 +0800

    Webserver working - can list on or receives requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{
+    fmt::Error,
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
